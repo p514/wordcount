74ee2dcde42e3f258d79c9426e79f75bf9063a53
Popov <vpopov@amt.ru>
2022-04-05 15:48:14 +0300

    simplified

--- wordcount.go
+++ wordcount.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-	"unicode"
+    "fmt"
+    "os"
+    "strings"
 )
 
-func count(src string) int {
-	f := func(c rune) bool {
-		return !unicode.IsSpace(c)
-	}
-	return len(strings.FieldsFunc(src, f))
-}
-
 func main() {
-	src := readInput()
-	if src == "" {
-		fmt.Println(0)
-	} else {
-		n := count(src) + 1
-		fmt.Println(n)
-	}	
-}
-
-func readInput() string {
-	return os.Args[1]	
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: wordcount PHRASE")
+        os.Exit(1)
+    }
+    fmt.Println(len(strings.Fields(os.Args[1])))
 }
